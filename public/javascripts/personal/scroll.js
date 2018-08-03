1b96153484687cb2d43f8d65540dd59a973a2523
Nino67 <gricca1967@gmail.com>
2018-08-03 16:35:41 -0400

    just a development github push

--- public/javascripts/personal/scroll.js
+++ public/javascripts/personal/scroll.js
@@ -14,7 +14,9 @@
     .addTo(controller);
     // TweenMax.to("#content-scene-0", 10, {opacity:1});
 
-
+    var barWidth = (windo.innerWidth / 5);
+    console.log(barWidth);
+    
     var options = {
       horizontalPixelsCount: (window.innerWidth / 5),
       verticalPixelsCount: 5,
